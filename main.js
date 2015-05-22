497664a6f02f39d2d999fa56bec492e3d8cce9bb
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-22 13:29:53 -0400

    Filter Etsy data by price

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert("hello");
+
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=l16o96t6qsh5xw9lwh1itw9x&keywords=baseball&sort_on=price&includes=Images,Shop"
+fetchJSONP(url, logResults);
+
+
+
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
