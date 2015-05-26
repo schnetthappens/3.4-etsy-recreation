2d1f7d84a0bd47271d4b49e68ec2be1b067f74cb
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-25 22:03:45 -0400

    Add category listing placeholders

--- main.js
+++ main.js
@@ -40,3 +40,34 @@
           ulElement.insertAdjacentHTML('beforeend', output);
         });
       }
+
+
+// (function(){
+//   'use strict';
+//
+// })();
+//
+//
+// var url = "https://api.etsy.com/v2/listings/active.js?api_key=l16o96t6qsh5xw9lwh1itw9x&keywords=baseball&includes=Images,Shop"
+// fetchJSONP(url, returnProducts);
+//
+//
+// function fetchJSONP(url, callback) {
+//     var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+//     var script = document.createElement('script');
+//
+//     window[callbackName] = function(data) {
+//         delete window[callbackName];
+//         document.body.removeChild(script);
+//         callback(data);
+//     };
+//
+//     script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+//     document.body.appendChild(script);
+// }
+//
+//
+// function returnProducts(response) {
+//     var products = response.results;
+//     console.log(products);
+//   }
